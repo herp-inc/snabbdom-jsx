68d011f3e59d20bd13e2f97c69840c252d9b5211
Ryota Kameoka <ok@ryota-ka.me>
2023-10-19 21:57:00 +0900

    Declare `JSX` namespace locally

--- jsx-runtime.d.ts
+++ jsx-runtime.d.ts
@@ -1,9 +1,7 @@
 import { jsx } from '@herp-inc/snabbdom-jsx';
 
-declare global {
-    namespace JSX {
-        type Element = jsx.JSX.Element;
-        type IntrinsicAttributes = jsx.JSX.IntrinsicAttributes;
-        type IntrinsicElements = jsx.JSX.IntrinsicElements;
-    }
+export declare namespace JSX {
+    type Element = jsx.JSX.Element;
+    type IntrinsicAttributes = jsx.JSX.IntrinsicAttributes;
+    type IntrinsicElements = jsx.JSX.IntrinsicElements;
 }
